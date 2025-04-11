1f24541c5a9fc6a9083ff0914054699aa3cc0739
BakangMonei <bakangmonei2@gmail.com>
2025-04-11 18:29:31 +0200

    Adding tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts,tsx}"
+  ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Lexend', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+    },
+    screens: {
+      'xs': '480px', 
+      'sm': '640px',
+      'md': '868px',
+      'lg': '1024px', 
+      'xl': '1280px',
+    },
   },
   plugins: [],
 }
-
